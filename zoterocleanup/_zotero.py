2ec8c83afc3934c593dc453c605927a5d280a65e
Christian Brodbeck <christianmbrodbeck@gmail.com>
2017-05-08 15:48:12 -0400

    ENH connect:  remember library

--- zoterocleanup/_zotero.py
+++ zoterocleanup/_zotero.py
@@ -1,28 +1,74 @@
 # Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+from configparser import SafeConfigParser
 from datetime import datetime
-import sys
+from os.path import exists, expanduser, join
 
 import keyring
-from pyzotero import zotero
+from pyzotero.zotero import Zotero
+from pyzotero.zotero_errors import UserNotAuthorised
 
 DATE_FMT = "%Y-%m-%dT%XZ"
 KEYRING_DOMAIN = "Zotero Cleanup Library"
+CONFIG_PATH = join(expanduser('~'), '.zotero_cleanup.cfg')
 
 
-def connect(library=None):
+def connect(library=None, api_key=None):
+    config_library = get_library()
     if library is None:
-        library = raw_input("Library ID: ")
+        library = config_library
     else:
         library = str(library)
-    password = keyring.get_password(KEYRING_DOMAIN, library)
-    if password is None:
-        password = raw_input("Library Password: ")
-        if password:
-            keyring.set_password(KEYRING_DOMAIN, library, password)
+    # user input
+    if library is None:
+        while True:
+            library = raw_input("Library ID: ")
+            if library and library.isdigit():
+                break
+            print("Library needs to be a sequence of digits, not %r" % library)
+
+    if api_key is None:
+        api_key = keyring.get_password(KEYRING_DOMAIN, library)
+        api_key_changed = False
+    else:
+        api_key_changed = True
+
+    msg_printed = False
+    while True:
+        if not api_key:
+            if not msg_printed:
+                print("Please enter the library API key "
+                      "(see https://www.zotero.org/settings/keys/new)")
+                msg_printed = True
+            api_key = raw_input("Library API key (ctrl-c to abort): ")
+
+        z = Zotero(library, 'user', api_key, True)
+        try:
+            z.num_items()
+        except UserNotAuthorised:
+            print("Connection refused, invalid API key...")
+            api_key = None
         else:
-            sys.exit()
-    return zotero.Zotero(library, 'user', password, True)
+            # store new configuration
+            if library != config_library:
+                set_library(library)
+            if api_key_changed:
+                keyring.set_password(KEYRING_DOMAIN, library, api_key)
+            return z
 
 
 def date_added(item):
     return datetime.strptime(item['data']['dateAdded'], DATE_FMT)
+
+
+def get_library():
+    config = SafeConfigParser()
+    if exists(CONFIG_PATH):
+        config.read(CONFIG_PATH)
+        return config.get('library', 'id')
+
+
+def set_library(library):
+    config = SafeConfigParser()
+    config.add_section('library')
+    config.set('library', 'id', library)
+    config.write(open(CONFIG_PATH, 'w'))
